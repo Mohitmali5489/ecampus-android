4d2d91759bbf91e558062d13f9c69415d8455c87
Mohit Mali <115642626+Mohitmali5489@users.noreply.github.com>
2026-08-04 13:28:10 +0530

    Delete android directory

--- android/app/src/main/java/com/mohit/ecampus/MainActivity.java
+++
@@ -1,5 +0,0 @@
-find android/app/src/main -name "AndroidManifest.xml" -o -name "MainActivity.java"package com.mohit.ecampus;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
